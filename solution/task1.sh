2aafbebddcf2d2ae9ac90ef8d1a53286c95b1c43
iwawaq <poshchadite@gmail.com>
2025-09-09 21:51:57 +0700

    Update solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,4 @@
-cd ~
-cd mpt-command-line-basics/data/
 mkdir new_dir
-tar -xf archive-part1.tar -C new_dir
-unzip archive-part2.zip -d new_dir
-tar -czf archive-combined.tar.gz new_dir
+tar -xf data/archive-part1.tar -C new_dir
+unzip data/archive-part2.zip -d new_dir
+tar -czf data/archive-combined.tar.gz new_dir
