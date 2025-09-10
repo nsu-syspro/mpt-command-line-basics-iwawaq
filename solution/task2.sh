6116e31a449adf2f31c961b6552e574e5a12d2c0
iwawaq <poshchadite@gmail.com>
2025-09-10 18:09:42 +0700

    helpfgdfgfiwe

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,9 +1,4 @@
-mkdir new_dir1
-unzip archive-part2.zip -d new_dir1
-tar -xvf archive-part1.tar -C new_dir1
 
-cat $(find new_dir1/ -type f -name foo.txt)
-cat $(find new_dir1/ -type f -name bar.txt)
-cat $(find new_dir1/ -type f -name baz.txt)
-
-rm -rf new_dir1	
+cat $(. -type f -name foo.txt)
+cat $(. -type f -name bar.txt)
+cat $(. -type f -name baz.txt)
