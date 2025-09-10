de5e1650a613b9b2e233ce7f1a87f0a661bda3a2
iwawaq <poshchadite@gmail.com>
2025-09-10 17:27:02 +0700

    Update solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,6 @@
 cd data/
 mkdir temp_dir1
 unzip archive-part2.zip -d temp_dir1
-tar -xf archive-part1.tar temp_dir1
-tar -czf archive-combined.tar.gz temp_dir1
+tar -xf archive-part1.tar -C temp_dir1
+tar -czf archive-combined.tar.gz -C temp_dir1
 rm -rf temp_dir1
