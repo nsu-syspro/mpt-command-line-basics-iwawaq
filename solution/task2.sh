393e2623a9501e1befb2a88fc05e097518e78aab
iwawaq <poshchadite@gmail.com>
2025-09-10 15:44:05 +0700

    Update solution

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,9 +1,4 @@
-cd ~
-cd mpt-command-line-basics-iwawaq/data/
-mkdir dir_task2
-tar -xf archive-part1.tar -C dir_task2
-unzip archive-part2.zip -d dir_task2
 
-cat $(find dir_task2 -type f -name foo.txt)
-cat $(find dir_task2 -type f -name bar.txt)
-cat $(find dir_task2 -type f -name baz.txt)
+cat $(find data -type f -name foo.txt)
+cat $(find data -type f -name bar.txt)
+cat $(find data -type f -name baz.txt)
