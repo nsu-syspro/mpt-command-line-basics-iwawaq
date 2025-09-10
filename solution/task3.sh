cd6a21df6c68cb4b857b48c1dc3b81de4ef57954
iwawaq <poshchadite@gmail.com>
2025-09-10 18:14:33 +0700

    solution?

--- solution/task3.sh
+++ solution/task3.sh
@@ -1,6 +1,1 @@
-cd ~
-cd mpt-command-line-basics-iwawaq/data/
-mkdir dir_task3
-tar -xf archive-part1.tar -C dir_task3
-unzip archive-part2.zip -d dir_task3
 basename -a $(find dir_task3 -type f -name "*.txt") | sort
