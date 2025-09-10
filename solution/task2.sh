04bc737a3a1ab53aa99d4dc672f9e333a7cd2d59
iwawaq <poshchadite@gmail.com>
2025-09-10 17:54:23 +0700

    fgophdfpgh

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,4 +1,4 @@
-cd data/
+
 mkdir new_dir1
 unzip archive-part2.zip -d new_dir1
 tar -xvf archive-part1.tar -C new_dir1
@@ -7,4 +7,4 @@ cat $(find new_dir1/ -type f -name foo.txt)
 cat $(find new_dir1/ -type f -name bar.txt)
 cat $(find new_dir1/ -type f -name baz.txt)
 
-rm -rf new_dir1
+rm -rf new_dir1	
