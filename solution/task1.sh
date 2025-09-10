21b98ee069cf510c51ed5d13f29e88151dfc6569
iwawaq <poshchadite@gmail.com>
2025-09-10 15:20:12 +0700

    Update solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,4 +1,4 @@
 mkdir new_dir
 tar -xf data/archive-part1.tar -C new_dir
 unzip data/archive-part2.zip -d new_dir
-tar -czf data/archive-combined.tar.gz -C new_dir
+tar -czf data/archive-combined.tar.gz new_dir
