6b0d945f336d1ee224fe0da21773a117638f609f
iwawaq <poshchadite@gmail.com>
2025-09-10 17:43:27 +0700

    Update solution

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,6 +1,10 @@
+cd data/
+mkdir new_dir1
+unzip archive-part2.zip -d new_dir1
+tar -xvf archive-part1.tar -C new_dir1
 
-cd ~
-cd mpt-command-line-basics/data/
-cat $(find data -type f -name foo.txt)
-cat $(find data -type f -name bar.txt)
-cat $(find data -type f -name baz.txt)
+cat $(find new_dir1/ -type f -name foo.txt)
+cat $(find new_dir1/ -type f -name bar.txt)
+cat $(find new_dir1/ -type f -name baz.txt)
+
+rm -rf new_dir1
